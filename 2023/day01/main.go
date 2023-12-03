28c46535c478f1a9f63cd71ec6fb54c9b0ff5d17
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-03 17:26:39 +0200

    feat: add 2023 day01 part2

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -30,6 +30,9 @@ func main() {
     case 1:
         output := part1(input)
         fmt.Printf("Output: %v\n", output)
+    case 2:
+        output := part2(input)
+        fmt.Printf("Output: %v\n", output)
     default:
         panic(fmt.Sprintf("I don't know nothing about this part. Is there a part %v?", part))
     }
@@ -53,6 +56,75 @@ func part1(input string) int {
                     lastDigit = int(character - '0')
                 }
             }
+        }
+
+        if digitsFound {
+            sum += firstDigit*10 + lastDigit
+        }
+    }
+
+    return sum
+}
+
+func part2(input string) int {
+    var sum int = 0
+    digits := map[string]int{
+        "one":   1,
+        "two":   2,
+        "three": 3,
+        "four":  4,
+        "five":  5,
+        "six":   6,
+        "seven": 7,
+        "eight": 8,
+        "nine":  9,
+        "1":     1,
+        "2":     2,
+        "3":     3,
+        "4":     4,
+        "5":     5,
+        "6":     6,
+        "7":     7,
+        "8":     8,
+        "9":     9,
+    }
+
+    existsInDigits := func(maybeDigitString string) bool {
+        for digitString := range digits {
+            if strings.HasPrefix(digitString, maybeDigitString) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    lines := strings.Split(input, "\n")
+    for _, line := range lines {
+        var firstDigit, lastDigit int
+        var digitsFound bool = false
+
+        i := 0
+        for i < len(line) {
+            j := i + 1
+            for existsInDigits(line[i:j]) {
+                j += 1
+                if j == len(line)+1 {
+                    break
+                }
+            }
+
+            digit, ok := digits[line[i:j-1]]
+            if ok {
+                if !digitsFound {
+                    firstDigit = digit
+                    lastDigit = digit
+                    digitsFound = true
+                } else {
+                    lastDigit = digit
+                }
+            }
+            i += 1
         }
 
         if digitsFound {
--- 2023/day01/main_test.go
+++ 2023/day01/main_test.go
@@ -35,3 +35,38 @@ func TestPart1(t *testing.T) {
         })
     }
 }
+
+var examplePart2Input string = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+func TestPart2(t *testing.T) {
+    var tests = []struct {
+        name  string
+        given string
+        want  int
+    }{
+        {
+            name:  "example",
+            given: examplePart2Input,
+            want:  281,
+        },
+        {
+            name:  "puzzle",
+            given: input,
+            want:  54100,
+        },
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := part2(tt.given); got != tt.want {
+                t.Errorf("part2() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}

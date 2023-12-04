742b1146dc418b3d0dd2ab7aa001b9d7c3957ede
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-04 13:08:47 +0200

    feat: add 2023 day02 part1

---
+++ 2023/day02/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+    _ "embed"
+    "flag"
+    "fmt"
+    "strconv"
+    "strings"
+)
+
+//go:embed input
+var input string
+
+func init() {
+    input = strings.Trim(input, " \n")
+    if len(input) == 0 {
+        panic("input can't be empty")
+    }
+}
+
+func main() {
+    var part int
+
+    flag.IntVar(&part, "part", 1, "part 1 or 2")
+    flag.Parse()
+
+    fmt.Println("Running Part", part)
+
+    switch part {
+    case 1:
+        output := part1(input)
+        fmt.Printf("Output: %v\n", output)
+    default:
+        panic(fmt.Sprintf("I don't know nothing about this part. Is there a part %v?", part))
+    }
+}
+
+func convStrToInt(valueStr string) int {
+    value, err := strconv.Atoi(strings.Trim(valueStr, " "))
+    if err != nil {
+        panic(err)
+    }
+
+    return value
+}
+
+func part1(input string) int {
+    var sum int
+
+    lines := strings.Split(input, "\n")
+
+    for _, lineRaw := range lines {
+        split := strings.SplitN(lineRaw, ":", 2)
+        gameRaw, setsRaw := split[0], split[1]
+
+        split = strings.SplitN(gameRaw, " ", 2)
+        gameIdRaw := split[1]
+        gameId := convStrToInt(gameIdRaw)
+
+        sum += gameId
+
+        sets := strings.Split(strings.Trim(setsRaw, " "), ";")
+        for _, setRaw := range sets {
+            var red, green, blue int
+
+            colors := strings.Split(setRaw, ",")
+            for _, colorRaw := range colors {
+                colorRaw = strings.Trim(colorRaw, " ")
+                split = strings.SplitN(colorRaw, " ", 2)
+                amountRaw, color := split[0], split[1]
+                amount := convStrToInt(amountRaw)
+
+                if color == "red" {
+                    red = amount
+                } else if color == "green" {
+                    green = amount
+                } else if color == "blue" {
+                    blue = amount
+                }
+            }
+
+            if red > 12 || green > 13 || blue > 14 {
+                sum -= gameId
+                break
+            }
+        }
+    }
+
+    return sum
+}
---
+++ 2023/day02/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "testing"
+)
+
+var examplePart1Input string = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+func TestPart1(t *testing.T) {
+    var tests = []struct {
+        name  string
+        given string
+        want  int
+    }{
+        {
+            name:  "example",
+            given: examplePart1Input,
+            want:  8,
+        },
+        {
+            name:  "puzzle",
+            given: input,
+            want:  2486,
+        },
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            if got := part1(tt.given); got != tt.want {
+                t.Errorf("part1() = %v, want %v", got, tt.want)
+            }
+        })
+    }
+}
